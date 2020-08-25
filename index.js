ad0891fda5b8c95e521d553192a39f8684f2b7cb
hellbound <mariovparaschiv@gmail.com>
2020-08-25 18:23:28 +0100

    oops & enforce coding style

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const { Plugin } = require('powercord/entities')
-const { inject, uninject } = require('powercord/injector')
-const { getModuleByDisplayName } = require('powercord/webpack')
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { getModuleByDisplayName } = require('powercord/webpack');
 
 module.exports = class extends Plugin {
    async startPlugin() {
-      const ChannelItem = getModuleByDisplayName('ChannelItem', false)
+      const ChannelItem = getModuleByDisplayName('ChannelItem', false);
       inject('double-click-vc', ChannelItem.prototype, 'render', (args, res) => {
-         const channel = this.getNestedProp(res, 'props.children.1.props.children.1.props.children.1.props.channel')
+         const channel = this.getNestedProp(res, 'props.children.1.props.children.1.props.children.1.props.channel');
          if (channel && channel.type == 2) {
-            const props = this.getNestedProp(res, 'props.children.1.props.children.0.props')
+            const props = this.getNestedProp(res, 'props.children.1.props.children.0.props');
             if (props) {
-               const onClick = props.onClick
-               props.onDoubleClick = onClick
-               props.onClick = () => {}
+               const onClick = props.onClick;
+               props.onDoubleClick = onClick;
+               props.onClick = () => {};
             } else {
-               this.log('Failed to get nested props.')
+               this.log('Failed to get nested props.');
             }
-         } else if(!channel) { {
-               this.log('Failed to determine channel type.')
+         } else if (!channel) {
+            {
+               this.log('Failed to determine channel type.');
+            }
+            return res;
          }
-         return res
-      })
+      });
    }
 
    pluginWillUnload() {
-      uninject('double-click-vc')
+      uninject('double-click-vc');
    }
 
    getNestedProp(obj, path) {
       return path.split('.').reduce(function (obj, prop) {
-         return obj && obj[prop]
-      }, obj)
+         return obj && obj[prop];
+      }, obj);
    }
-}
+};
