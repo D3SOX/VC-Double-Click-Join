b5df583cd363f9fcdbc77d619431310f3024d8d8
D3SOX <d3sox@protonmail.com>
2021-08-12 18:48:32 +0200

    refactor: format and remove unnecessary variable

--- index.js
+++ index.js
@@ -3,36 +3,35 @@ const { inject, uninject } = require('powercord/injector');
 const { getModule } = require('powercord/webpack');
 
 module.exports = class DoubleClickVc extends Plugin {
-   async startPlugin() {
-      const ChannelItem = await getModule(m => m.default && m.default.displayName === 'ChannelItem');
-      const oDefault = ChannelItem.default
-      inject('double-click-vc', ChannelItem, 'default', (args, res) => {
-         const channel = this.getNestedProp(res, 'props.children.props.children.1.props.children.1.props.children.1.props.channel');
-         if (channel && (channel.type === 2 || channel.type === 13)) {
-            const props = this.getNestedProp(res, 'props.children.props.children.1.props.children.0.props');
-            if (props) {
-               const onClick = props.onClick;
-               props.onDoubleClick = onClick;
-               props.onClick = () => {};
-            } else {
-               this.log('Failed to get nested props.');
+    async startPlugin() {
+        const ChannelItem = await getModule(m => m.default && m.default.displayName === 'ChannelItem');
+        const oDefault = ChannelItem.default;
+        inject('double-click-vc', ChannelItem, 'default', (args, res) => {
+            const channel = this.getNestedProp(res, 'props.children.props.children.1.props.children.1.props.children.1.props.channel');
+            if (channel && (channel.type === 2 || channel.type === 13)) {
+                const props = this.getNestedProp(res, 'props.children.props.children.1.props.children.0.props');
+                if (props) {
+                    props.onDoubleClick = props.onClick;
+                    delete props.onClick;
+                } else {
+                    this.log('Failed to get nested props.');
+                }
+            } else if (!channel) {
+                this.log('Failed to determine channel type.');
             }
-         } else if (!channel) {
-            this.log('Failed to determine channel type.');
-         }
 
-         return res
-      });
-      Object.assign(ChannelItem.default, oDefault)
-   }
+            return res;
+        });
+        Object.assign(ChannelItem.default, oDefault);
+    }
 
-   pluginWillUnload() {
-      uninject('double-click-vc');
-   }
+    pluginWillUnload() {
+        uninject('double-click-vc');
+    }
 
-   getNestedProp(obj, path) {
-      return path.split('.').reduce(function (obj, prop) {
-         return obj && obj[prop];
-      }, obj);
-   }
+    getNestedProp(obj, path) {
+        return path.split('.').reduce(function (obj, prop) {
+            return obj && obj[prop];
+        }, obj);
+    }
 };
